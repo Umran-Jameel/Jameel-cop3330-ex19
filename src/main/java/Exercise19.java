a694b6504c0eba9889ed2dcfb7974a6c528db147
Umran Jameel <umranrazack02@mak-bwk-lmran.local>
2021-06-06 18:18:23 -0400

    Initial commit

---
+++ src/main/java/Exercise19.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Exercise19 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+
+
+        System.out.print("What is you height (inches)? ");
+        String inches = in.nextLine();
+        double height = Double.parseDouble(inches);
+
+
+        System.out.print("What is your weight (pounds)? ");
+        String pounds = in.nextLine();
+        double weight = Double.parseDouble(pounds);
+
+
+        double bmi = (weight / (height * height)) * 703;
+
+        if (bmi < 18.5) {
+            System.out.printf("Your BMI is %.1f\nYou should see your doctor.", bmi);
+        }
+        else if (bmi < 25) {
+            System.out.printf("Your BMI is %.1f\nYou are within the ideal weight range.", bmi);
+        }
+        else {
+            System.out.printf("Your BMI is %.1f\nYou are overweight, you should see your doctor.", bmi);
+        }
+    }
+}
